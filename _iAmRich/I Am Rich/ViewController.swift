3d0986d9abc150948e07f80ca22eb649e5f4132d
melofo <51918740+melofo@users.noreply.github.com>
2020-06-29 12:05:22 -0700

    Add original files.

---
+++ _iAmRich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by melofo on 6/29/20.
+//  Copyright © 2020 melofo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
